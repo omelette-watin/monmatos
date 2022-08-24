2fb9c3eda573f2487be8ac150f31e32c370fed01
Nicolas <pnwa@icloud.com>
2022-08-24 12:16:07 +0200

    chore: add react icons

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,18 +1,22 @@
 import { UIProps } from "@/utils/typedProps"
 import * as HeroIcons from "@heroicons/react/outline"
+import * as ReactIcons from "react-icons/all"
 import { FC } from "react"
 import cn from "classnames"
 
-export type IconName = keyof typeof HeroIcons
+export type HeroIconsName = keyof typeof HeroIcons
+export type ReactIconsName = keyof typeof ReactIcons
+export type IconName = HeroIconsName | ReactIconsName
 
 const Icon: FC<UIProps<{ name: IconName }, SVGSVGElement>> = ({
   name,
   className,
   ...otherProps
 }) => {
-  const HeroIcon = HeroIcons[name as IconName]
+  const Icon =
+    HeroIcons[name as HeroIconsName] || ReactIcons[name as ReactIconsName]
 
-  return <HeroIcon {...otherProps} className={cn("w-6", className)} />
+  return <Icon {...otherProps} className={cn("w-6", className)} />
 }
 
 export default Icon
