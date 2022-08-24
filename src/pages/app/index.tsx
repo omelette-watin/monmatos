193c6d896a97b9c3d86ee3ecbf9cb297b96a8e02
Nicolas <pnwa@icloud.com>
2022-08-24 23:55:58 +0200

    chore: add action panel to dashboard

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,9 +1,27 @@
+import ActionsPanel from "@/components/business/dashboard/ActionsPanel"
 import MainLayout from "@/components/ui/layouts/MainLayout"
+import { trpc } from "@/utils/trpc"
+import { useSession } from "next-auth/react"
 import { ReactElement } from "react"
 import { NextPageWithLayout } from "../_app"
 
 const GroupPage: NextPageWithLayout = () => {
-  return <div>Group Page</div>
+  const { data } = useSession()
+  const { data: tents } = trpc.useQuery(["tents.getAll"])
+
+  return (
+    <div className="space-y-10">
+      <h1 className="text-4xl font-bold lg:text-5xl">
+        <span>Groupe </span>
+        <span className="text-emerald-600">{data?.user?.name}</span>
+      </h1>
+      {tents && data && (
+        <>
+          <ActionsPanel session={data} tents={tents} />
+        </>
+      )}
+    </div>
+  )
 }
 
 GroupPage.getLayout = (page: ReactElement) => (
