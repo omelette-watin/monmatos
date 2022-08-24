c55e94b0aa5e530cc3e98ef771b697bf1ca24c27
Nicolas <pnwa@icloud.com>
2022-08-24 16:11:23 +0200

    chore: add id and scroll margin top

--- src/components/ui/Panel.tsx
+++ src/components/ui/Panel.tsx
@@ -2,20 +2,16 @@ import { UIProps } from "@/utils/typedProps"
 import classNames from "classnames"
 import { FC, ReactNode } from "react"
 
-const Panel: FC<UIProps<{ children: ReactNode; className?: string }>> = ({
-  children,
-  className,
-  ...otherProps
-}) => {
+const Panel: FC<
+  UIProps<{ children: ReactNode; className?: string; id?: string }>
+> = ({ children, className, id, ...otherProps }) => {
   return (
     <section
-      className={classNames(
-        "space-y-6 rounded-xl border-4 bg-slate-50 p-4",
-        className,
-      )}
+      className={classNames("rounded-xl border-4 bg-slate-50 p-4", className)}
       {...otherProps}
     >
-      {children}
+      {id && <span id={id} className="invisible scroll-mt-[110px]" />}
+      <div className="space-y-6">{children}</div>
     </section>
   )
 }
