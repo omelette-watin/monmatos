4da2c44f37fecebb6997709f2491f6f53e85e66d
Nicolas <pnwa@icloud.com>
2022-08-27 19:05:17 +0200

    chore: move tents to app context

--- src/components/business/AppContext.tsx
+++ src/components/business/AppContext.tsx
@@ -1,3 +1,4 @@
+import { inferQueryOutput, trpc } from "@/utils/trpc"
 import { UIProps } from "@/utils/typedProps"
 import {
   createContext,
@@ -5,16 +6,22 @@ import {
   FC,
   ReactNode,
   SetStateAction,
+  useEffect,
   useState,
 } from "react"
 import { Modal } from "../ui/modal"
 import { Notification } from "./Notification"
+
+export type Tents = inferQueryOutput<"tents.getAll">
 
 export type AppContext = {
   notification: Notification
   setNotification: Dispatch<SetStateAction<Notification>>
   modal: Modal
   setModal: Dispatch<SetStateAction<Modal>>
+  tents: Tents
+  setTents: Dispatch<SetStateAction<Tents>>
+  isLoadingTents: boolean
 }
 
 export const AppContext = createContext<AppContext>({} as AppContext)
@@ -22,12 +29,28 @@ export const AppContext = createContext<AppContext>({} as AppContext)
 export const AppContextProvider: FC<UIProps<{ children: ReactNode }>> = ({
   children,
 }) => {
+  const { data, isLoading } = trpc.useQuery(["tents.getAll"])
   const [notification, setNotification] = useState({} as Notification)
   const [modal, setModal] = useState({} as Modal)
+  const [tents, setTents] = useState(data as Tents)
+
+  useEffect(() => {
+    if (data) {
+      setTents(data)
+    }
+  }, [data])
 
   return (
     <AppContext.Provider
-      value={{ notification, setNotification, modal, setModal }}
+      value={{
+        notification,
+        setNotification,
+        modal,
+        setModal,
+        tents,
+        setTents,
+        isLoadingTents: isLoading,
+      }}
     >
       {children}
     </AppContext.Provider>
