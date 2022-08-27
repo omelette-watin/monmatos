da3e07979ff9d6f47afb90f532abd9786d6c1122
Nicolas <pnwa@icloud.com>
2022-08-27 19:07:30 +0200

    chore: use tents from context

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,15 +1,15 @@
 import ActionsPanel from "@/components/business/dashboard/ActionsPanel"
 import OverviewPanel from "@/components/business/dashboard/OverviewPanel"
+import { useAppContext } from "@/components/ui/hooks/useAppContext"
 import MainLayout from "@/components/ui/layouts/MainLayout"
 import Loading from "@/components/ui/Loading"
-import { trpc } from "@/utils/trpc"
 import { useSession } from "next-auth/react"
 import { ReactElement } from "react"
 import { NextPageWithLayout } from "../_app"
 
 const GroupPage: NextPageWithLayout = () => {
   const { data } = useSession()
-  const { data: tents, isLoading } = trpc.useQuery(["tents.getAll"])
+  const { tents, isLoadingTents } = useAppContext()
 
   return (
     <div className="space-y-10">
@@ -17,7 +17,7 @@ const GroupPage: NextPageWithLayout = () => {
         <span>Groupe </span>
         <span className="text-emerald-600">{data?.user?.name}</span>
       </h1>
-      {isLoading && (
+      {isLoadingTents && (
         <div className="m-auto w-fit py-32">
           <Loading />
         </div>
