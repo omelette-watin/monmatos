5af29b1c29a82d2022c7aae43c748cf1edce6956
Nicolas <pnwa@icloud.com>
2022-08-31 21:17:55 +0200

    chore: skip connexion if group id is the same

--- src/pages/connexion.tsx
+++ src/pages/connexion.tsx
@@ -1,13 +1,17 @@
 import { ILogin } from "@/common/validation/auth"
+import Loading from "@/components/ui/Loading"
+import Logo from "@/components/ui/Logo"
 import Page from "@/components/ui/Page"
 import { Field, Form, Formik } from "formik"
 import { NextPage } from "next"
-import { signIn } from "next-auth/react"
+import { signIn, useSession } from "next-auth/react"
 import { useRouter } from "next/router"
 import { useCallback } from "react"
 
 const SignInPage: NextPage = () => {
-  const { query } = useRouter()
+  const router = useRouter()
+  const { query } = router
+  const { status, data: session } = useSession()
   const groupId = query.i as string
   const callbackUrl = (query.callbackUrl as string) || "/app"
   const credsError = query.error as string
@@ -21,27 +25,42 @@ const SignInPage: NextPage = () => {
   )
 
   if (groupId) {
-    handleSubmit({ identifier: groupId })
+    if (status !== "loading") {
+      if (session?.user?.id === groupId) {
+        router.push(callbackUrl)
+      } else {
+        handleSubmit({ identifier: groupId })
+      }
+    }
+  }
 
-    return <p className="min-h-screen bg-green-400">Loading</p>
+  if (!groupId) {
+    return (
+      <Page title="Connexion">
+        <Formik
+          initialValues={{
+            identifier: "ca5e5d0c-3bee-4269-9b9b-4e2372ffdc82",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form className="m-auto my-10 flex w-fit flex-col gap-3">
+            <Field name="identifier" className="border border-red-400" />
+            {errorMessage && (
+              <p className="font-bold text-red-500">{errorMessage}</p>
+            )}
+            <button type="submit">GO</button>
+          </Form>
+        </Formik>
+      </Page>
+    )
   }
 
   return (
     <Page title="Connexion">
-      <Formik
-        initialValues={{
-          identifier: "ca5e5d0c-3bee-4269-9b9b-4e2372ffdc82",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <Form className="m-auto my-10 flex w-fit flex-col gap-3">
-          <Field name="identifier" className="border border-red-400" />
-          {errorMessage && (
-            <p className="font-bold text-red-500">{errorMessage}</p>
-          )}
-          <button type="submit">GO</button>
-        </Form>
-      </Formik>
+      <div className="flex h-screen w-full flex-col items-center justify-start gap-24 pt-20">
+        <Logo size="lg" />
+        <Loading />
+      </div>
     </Page>
   )
 }
