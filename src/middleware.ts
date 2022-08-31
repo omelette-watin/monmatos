c0ee17ad95e746822352484c9be163f1ac0e068a
Nicolas <pnwa@icloud.com>
2022-08-31 20:51:16 +0200

    feat: redirect to app if request url is / and user authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,21 @@
 import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ token, req }) => {
-      if (req.nextUrl.pathname.startsWith("/app")) {
-        return token?.user.id
-      }
+export default withAuth(
+  (req) => {
+    if (req.nextauth.token && req.nextUrl.pathname === "/") {
+      return NextResponse.redirect(new URL("/app", req.url))
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        if (req.nextUrl.pathname.startsWith("/app")) {
+          return token?.user.id
+        }
 
-      return true
+        return true
+      },
     },
   },
-})
+)
