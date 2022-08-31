eb66a5bb476222098ebdd6148bba8e3a1fdca1a0
Nicolas <pnwa@icloud.com>
2022-08-31 21:02:51 +0200

    feat: add distinction between public header/sidebar and app's

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,134 +1,21 @@
-import { UIProps } from "@/utils/typedProps"
 import classNames from "classnames"
-import cn from "classnames"
 import { signOut } from "next-auth/react"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { FC, HTMLAttributeAnchorTarget, useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import Button from "./Button"
 import Container from "./Container"
+import HeaderLink from "./HeaderLink"
 import Icon from "./Icon"
 import Logo from "./Logo"
-
-const MenuLink: FC<
-  UIProps<
-    {
-      label: string
-      href: string
-      currentPath: string
-      target?: HTMLAttributeAnchorTarget
-    },
-    "a"
-  >
-> = ({ href, label, currentPath, className, target }) => {
-  const isActive = currentPath === href
-
-  return (
-    <Link href={href}>
-      <a
-        target={target}
-        className={cn("transition-colors hover:text-emerald-600", className, {
-          "text-black": !isActive,
-          "text-emerald-600": isActive,
-        })}
-      >
-        {label}
-      </a>
-    </Link>
-  )
-}
-
-const MobileSideMenu = () => {
-  const router = useRouter()
-  const asPath = router.asPath
-  const [visible, setVisible] = useState(false)
-  const showMenu = () => setVisible(true)
-  const hideMenu = () => setVisible(false)
-
-  useEffect(() => {
-    hideMenu()
-  }, [router])
-
-  return (
-    <div className="flex items-center lg:hidden">
-      <button type="button" onClick={showMenu}>
-        <Icon name="MenuIcon" />
-      </button>
-      <div
-        className={cn(
-          "bg-main fixed top-0 left-0 bottom-0 z-50 w-4/5 max-w-[100vw] overflow-y-scroll overscroll-contain shadow-xl transition-transform duration-500 sm:w-1/3",
-          {
-            "-translate-x-full": !visible,
-            "translate-x-0": visible,
-          },
-        )}
-      >
-        <div className="flex h-full w-full flex-col items-start">
-          <div className="flex w-full items-center justify-between p-4 shadow-md">
-            <Link href="/app">
-              <a>
-                <Logo size="sm" />
-              </a>
-            </Link>
-
-            <button type="button" onClick={hideMenu}>
-              <Icon name="XIcon" />
-            </button>
-          </div>
-
-          <div className="flex flex-col items-start space-y-3 p-6">
-            <MenuLink label="Mon groupe" href={"/app"} currentPath={asPath} />
-            <MenuLink
-              label="Mes tentes"
-              href={"/app/tentes"}
-              currentPath={asPath}
-            />
-            <MenuLink
-              label="Bien commencer"
-              href={"/guide"}
-              currentPath={asPath}
-              target="_blank"
-            />
-          </div>
-          <div className="flex flex-col items-start space-y-3 p-6">
-            <MenuLink
-              label="Nous contacter"
-              href={"/contact"}
-              currentPath={asPath}
-              target="_blank"
-            />
-            <MenuLink
-              label="Nous soutenir"
-              href={"/nous-soutenir"}
-              currentPath={asPath}
-              target="_blank"
-            />
-            <button
-              type="button"
-              className="text-red-600"
-              onClick={() => signOut({ callbackUrl: "/connexion" })}
-            >
-              Déconnexion
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        className={cn(
-          "fixed inset-0 z-40 bg-slate-900/50 transition-all duration-500",
-          {
-            "invisible opacity-0": !visible,
-            "visible opacity-100": visible,
-          },
-        )}
-        onClick={hideMenu}
-      />
-    </div>
-  )
-}
+import MobileSidebar from "./MobileSidebar"
 
 const Header = () => {
-  const { asPath } = useRouter()
+  const router = useRouter()
+  const { asPath } = router
+  const isPublic = !asPath.startsWith("/app")
   const [scrolled, setScrolled] = useState(false)
+  const goToConnexion = useCallback(() => router.push("/connexion"), [router])
 
   useEffect(() => {
     const handleScroll = () => setScrolled(window.scrollY > 0)
@@ -163,46 +50,74 @@ const Header = () => {
                 </a>
               </Link>
             </div>
-            <div className="ml-10 hidden items-center gap-4 sm:flex">
-              <MenuLink label="Mon groupe" href={"/app"} currentPath={asPath} />
-              <MenuLink
-                label="Mes tentes"
-                href={"/app/tentes"}
-                currentPath={asPath}
-              />
-              <MenuLink
-                label="Bien commencer"
-                href={"/guide"}
-                className="hidden md:block"
-                currentPath={asPath}
-                target="_blank"
-              />
+            <div className="ml-10 hidden items-center gap-6 sm:flex">
+              {isPublic ? (
+                <>
+                  <HeaderLink label="Guide" href="/guide" />
+                  <HeaderLink label="FAQ" href="/foire-aux-questions" />
+                  <HeaderLink label="Inscrire mon groupe" href="/inscription" />
+                </>
+              ) : (
+                <>
+                  <HeaderLink label="Mon groupe" href={"/app"} />
+                  <HeaderLink label="Mes tentes" href="/app/tentes" />
+                  <HeaderLink label="Guide" href="/guide" target="_blank" />
+                  <HeaderLink
+                    label="FAQ"
+                    href="/foire-aux-questions"
+                    target="_blank"
+                  />
+                </>
+              )}
             </div>
           </div>
           <div className="hidden items-center divide-x-2 lg:flex">
-            <MenuLink
-              label="Nous contacter"
-              href={"/contact"}
-              currentPath={asPath}
-              className="px-4"
-              target="_blank"
-            />
-            <MenuLink
-              label="Nous soutenir"
-              href={"/nous-soutenir"}
-              currentPath={asPath}
-              className="px-4"
-              target="_blank"
-            />
-            <button
-              className="px-4"
-              type="button"
-              onClick={() => signOut({ callbackUrl: "/connexion" })}
-            >
-              <Icon name="LogoutIcon" />
-            </button>
+            {isPublic ? (
+              <>
+                <HeaderLink
+                  label="Nous contacter"
+                  href={"/contact"}
+                  className="px-4"
+                />
+                <HeaderLink
+                  label="Nous soutenir"
+                  href={"/nous-soutenir"}
+                  className="px-4"
+                />
+                <Button
+                  size="xxs"
+                  className="max-w-fit"
+                  variant="black"
+                  onClick={goToConnexion}
+                >
+                  Me connecter
+                </Button>
+              </>
+            ) : (
+              <>
+                <HeaderLink
+                  label="Nous contacter"
+                  href={"/contact"}
+                  className="px-4"
+                  target="_blank"
+                />
+                <HeaderLink
+                  label="Nous soutenir"
+                  href={"/nous-soutenir"}
+                  className="px-4"
+                  target="_blank"
+                />
+                <button
+                  className="px-4"
+                  type="button"
+                  onClick={() => signOut({ callbackUrl: "/connexion" })}
+                >
+                  <Icon name="LogoutIcon" />
+                </button>
+              </>
+            )}
           </div>
-          <MobileSideMenu />
+          <MobileSidebar />
         </div>
       </Container>
     </div>
---
+++ src/components/ui/MobileSidebar.tsx
@@ -0,0 +1,151 @@
+import classNames from "classnames"
+import { signOut } from "next-auth/react"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState, useEffect, useCallback } from "react"
+import Button from "./Button"
+import HeaderLink from "./HeaderLink"
+import Icon from "./Icon"
+import Logo from "./Logo"
+
+const MobileSidebar = () => {
+  const router = useRouter()
+  const asPath = router.asPath
+  const isPublic = !asPath.startsWith("/app")
+  const [visible, setVisible] = useState(false)
+  const showMenu = () => setVisible(true)
+  const hideMenu = () => setVisible(false)
+  const goToInscription = useCallback(
+    () => router.push("/inscription"),
+    [router],
+  )
+  const goToConnexion = useCallback(() => router.push("/connexion"), [router])
+
+  useEffect(() => {
+    hideMenu()
+  }, [router])
+
+  return (
+    <div className="flex items-center lg:hidden">
+      <button type="button" onClick={showMenu}>
+        <Icon name="MenuIcon" />
+      </button>
+      <div
+        className={classNames(
+          "bg-main fixed top-0 left-0 bottom-0 z-50 w-[280px] max-w-[100vw] overflow-y-scroll overscroll-contain text-lg shadow-xl transition-transform duration-500",
+          {
+            "-translate-x-full": !visible,
+            "translate-x-0": visible,
+          },
+        )}
+      >
+        <div className="flex h-full w-full flex-col items-start">
+          <div className="flex w-full items-center justify-between p-4 shadow-md">
+            <Link href="/app">
+              <a>
+                <Logo size="sm" />
+              </a>
+            </Link>
+
+            <button type="button" onClick={hideMenu}>
+              <Icon name="XIcon" />
+            </button>
+          </div>
+
+          <div className="flex flex-col items-start space-y-3 p-6">
+            {isPublic ? (
+              <>
+                <HeaderLink label="Guide" href="/guide" />
+                <HeaderLink label="FAQ" href="/foire-aux-questions" />
+                <HeaderLink label="Inscrire mon groupe" href="/inscription" />
+              </>
+            ) : (
+              <>
+                <HeaderLink label="Mon groupe" href={"/app"} />
+                <HeaderLink label="Mes tentes" href="/app/tentes" />
+                <HeaderLink label="Guide" href="/guide" target="_blank" />
+                <HeaderLink
+                  label="FAQ"
+                  href="/foire-aux-questions"
+                  target="_blank"
+                />
+              </>
+            )}
+          </div>
+          <div className="flex h-full w-full flex-col items-start justify-between gap-20 p-6">
+            {isPublic ? (
+              <>
+                <div className="flex flex-col items-start gap-3">
+                  <HeaderLink label="Nous contacter" href={"/contact"} />
+                  <HeaderLink label="Nous soutenir" href={"/nous-soutenir"} />
+                </div>
+
+                <div className="flex w-full flex-col items-center space-y-3">
+                  <Button
+                    type="button"
+                    size="xs"
+                    variant="black"
+                    className="-ml-1 max-w-fit"
+                    onClick={goToConnexion}
+                  >
+                    Me connecter
+                  </Button>
+                  <p className="text-base text-slate-500">ou</p>
+                  <Button
+                    type="button"
+                    size="xs"
+                    variant="blue"
+                    className="-ml-1 max-w-fit"
+                    onClick={goToInscription}
+                  >
+                    Inscrire mon groupe
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex flex-col items-start space-y-3">
+                  <HeaderLink
+                    label="Nous contacter"
+                    href={"/contact"}
+                    target="_blank"
+                  />
+                  <HeaderLink
+                    label="Nous soutenir"
+                    href={"/nous-soutenir"}
+                    target="_blank"
+                  />
+                </div>
+                <div className="flex w-full flex-col items-center gap-3">
+                  <Button
+                    type="button"
+                    size="xs"
+                    variant="black"
+                    className="-ml-1 max-w-fit"
+                    onClick={() => signOut({ callbackUrl: "/connexion" })}
+                    icon="LogoutIcon"
+                    iconPosition="right"
+                  >
+                    Déconnexion
+                  </Button>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      <div
+        className={classNames(
+          "fixed inset-0 z-40 bg-slate-900/50 transition-all duration-500",
+          {
+            "invisible opacity-0": !visible,
+            "visible opacity-100": visible,
+          },
+        )}
+        onClick={hideMenu}
+      />
+    </div>
+  )
+}
+
+export default MobileSidebar
