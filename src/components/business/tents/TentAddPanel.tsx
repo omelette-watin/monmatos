3d07cc01864ee9c62a6148c231b0d046b7f99aa9
Nicolas <pnwa@icloud.com>
2022-08-31 16:02:58 +0200

    chore: add page title to panels

--- src/components/business/tents/TentAddPanel.tsx
+++ src/components/business/tents/TentAddPanel.tsx
@@ -7,6 +7,7 @@ import { UIProps } from "@/utils/typedProps"
 import { units } from "@/utils/unit"
 import { Group, State, Unit } from "@prisma/client"
 import classNames from "classnames"
+import Head from "next/head"
 import { FC, FormEvent, useState } from "react"
 import TentInput from "./TentInput"
 
@@ -61,152 +62,162 @@ const TentAddPanel: FC<UIProps<{ movement?: Group["movement"] }>> = ({
   }
 
   return (
-    <form className="mx-auto max-w-[450px] space-y-6 py-4" onSubmit={handleAdd}>
-      <div
-        className={classNames(
-          "mx-auto flex h-28 w-28 items-center justify-center rounded-full border-4",
-          {
-            "border-slate-800 text-slate-800": !identifyingNum,
-
-            "border-emerald-500/90 text-emerald-500/90":
-              identifyingNum &&
-              !forbidenIdentifyingNumbers.includes(identifyingNum as number),
-            "border-red-500/90 text-red-500/90":
-              identifyingNum &&
-              forbidenIdentifyingNumbers.includes(identifyingNum as number),
-          },
-        )}
+    <>
+      <Head>
+        <title>Ajouter une tente | MonMatos</title>
+      </Head>
+      <form
+        className="mx-auto max-w-[450px] space-y-6 py-4"
+        onSubmit={handleAdd}
       >
-        <input
-          type="text"
-          autoFocus
-          className="w-[90px] rounded-lg border-2 border-dashed bg-transparent p-1 px-2 text-center text-3xl font-bold outline-none"
-          placeholder={"XX"}
-          onChange={(e) => setIdentifyingNum(parseInt(e.target.value))}
-          value={identifyingNum || ""}
-        />
-      </div>
-      <div className="pt-4">
         <div
           className={classNames(
-            "mx-auto flex w-fit items-center space-x-2 rounded-lg py-1 px-2 text-sm font-medium  sm:text-base",
+            "mx-auto flex h-28 w-28 items-center justify-center rounded-full border-4",
             {
-              "bg-amber-100 text-amber-800": !identifyingNum,
-              "bg-green-100 text-green-800":
+              "border-slate-800 text-slate-800": !identifyingNum,
+
+              "border-emerald-500/90 text-emerald-500/90":
                 identifyingNum &&
                 !forbidenIdentifyingNumbers.includes(identifyingNum as number),
-              "bg-red-100 text-red-800":
+              "border-red-500/90 text-red-500/90":
                 identifyingNum &&
                 forbidenIdentifyingNumbers.includes(identifyingNum as number),
             },
           )}
         >
-          <Icon name="MdOutlineErrorOutline" className="text-xl" />
-          <span>Choisissez un numéro de tente non attribué</span>
+          <input
+            type="text"
+            autoFocus
+            className="w-[90px] rounded-lg border-2 border-dashed bg-transparent p-1 px-2 text-center text-3xl font-bold outline-none"
+            placeholder={"XX"}
+            onChange={(e) => setIdentifyingNum(parseInt(e.target.value))}
+            value={identifyingNum || ""}
+          />
         </div>
-      </div>
+        <div className="pt-4">
+          <div
+            className={classNames(
+              "mx-auto flex w-fit items-center space-x-2 rounded-lg py-1 px-2 text-sm font-medium  sm:text-base",
+              {
+                "bg-amber-100 text-amber-800": !identifyingNum,
+                "bg-green-100 text-green-800":
+                  identifyingNum &&
+                  !forbidenIdentifyingNumbers.includes(
+                    identifyingNum as number,
+                  ),
+                "bg-red-100 text-red-800":
+                  identifyingNum &&
+                  forbidenIdentifyingNumbers.includes(identifyingNum as number),
+              },
+            )}
+          >
+            <Icon name="MdOutlineErrorOutline" className="text-xl" />
+            <span>Choisissez un numéro de tente non attribué</span>
+          </div>
+        </div>
 
-      <div>
-        <p className="text-lg font-bold">Informations</p>
-        <p>Cliquez sur les éléments afin de les modifier</p>
-      </div>
-      <div className="space-y-2">
-        <TentInput
-          label={`Attribué au${unit !== "GROUPE" ? "x" : ""}`}
-          value={unit}
-          setValue={(value) => setUnit(value as Unit)}
-          options={Object.entries(units[movement]).map(([key, value]) => [
-            key as Unit,
-            value,
-          ])}
+        <div>
+          <p className="text-lg font-bold">Informations</p>
+          <p>Cliquez sur les éléments afin de les modifier</p>
+        </div>
+        <div className="space-y-2">
+          <TentInput
+            label={`Attribué au${unit !== "GROUPE" ? "x" : ""}`}
+            value={unit}
+            setValue={(value) => setUnit(value as Unit)}
+            options={Object.entries(units[movement]).map(([key, value]) => [
+              key as Unit,
+              value,
+            ])}
+          />
+          <TentInput
+            label="Taille"
+            value={size.toString()}
+            setValue={(value) => {
+              const int = parseInt(value as string)
+              setSize(int)
+            }}
+            options={[
+              ["1", "1 place"],
+              ["2", "2 places"],
+              ["3", "3 places"],
+              ["4", "4 places"],
+              ["5", "5 places"],
+              ["6", "6 places"],
+              ["8", "8 places"],
+            ]}
+          />
+          <TentInput
+            label="ÉTAT"
+            value={state}
+            setValue={(value) => setState(value as State)}
+            options={[
+              ["BON", "BON"],
+              ["MAUVAIS", "MAUVAIS"],
+              ["INUTILISABLE", "INUTILISABLE"],
+              ["NEUF", "NEUF"],
+            ]}
+          />
+          <TentInput
+            label="Complète ?"
+            value={complete ? "OUI" : "NON"}
+            setValue={(value) => setComplete(value === "OUI" ? true : false)}
+            options={[
+              ["OUI", "OUI"],
+              ["NON", "NON"],
+            ]}
+          />
+          <TentInput
+            label="TYPE"
+            value={type.toUpperCase()}
+            setValue={setType}
+            options={[
+              ["CANADIENNE", "CANADIENNE"],
+              ["QUECHUA", "QUECHUA"],
+              ["MARABOUT", "MARABOUT"],
+            ]}
+          />
+          <TentInput
+            label="Tapis de sol"
+            value={integrated ? "INTÉGRÉ" : "NORMAL"}
+            setValue={(value) =>
+              setIntegrated(value === "INTÉGRÉ" ? true : false)
+            }
+            options={[
+              ["INTÉGRÉ", "INTÉGRÉ"],
+              ["NORMAL", "NORMAL"],
+            ]}
+          />
+        </div>
+        <textarea
+          value={comments}
+          onChange={(e) => setComments(e.target.value)}
+          placeholder="Commentaire"
+          className="w-full p-2"
         />
-        <TentInput
-          label="Taille"
-          value={size.toString()}
-          setValue={(value) => {
-            const int = parseInt(value as string)
-            setSize(int)
-          }}
-          options={[
-            ["1", "1 place"],
-            ["2", "2 places"],
-            ["3", "3 places"],
-            ["4", "4 places"],
-            ["5", "5 places"],
-            ["6", "6 places"],
-            ["8", "8 places"],
-          ]}
-        />
-        <TentInput
-          label="ÉTAT"
-          value={state}
-          setValue={(value) => setState(value as State)}
-          options={[
-            ["BON", "BON"],
-            ["MAUVAIS", "MAUVAIS"],
-            ["INUTILISABLE", "INUTILISABLE"],
-            ["NEUF", "NEUF"],
-          ]}
-        />
-        <TentInput
-          label="Complète ?"
-          value={complete ? "OUI" : "NON"}
-          setValue={(value) => setComplete(value === "OUI" ? true : false)}
-          options={[
-            ["OUI", "OUI"],
-            ["NON", "NON"],
-          ]}
-        />
-        <TentInput
-          label="TYPE"
-          value={type.toUpperCase()}
-          setValue={setType}
-          options={[
-            ["CANADIENNE", "CANADIENNE"],
-            ["QUECHUA", "QUECHUA"],
-            ["MARABOUT", "MARABOUT"],
-          ]}
-        />
-        <TentInput
-          label="Tapis de sol"
-          value={integrated ? "INTÉGRÉ" : "NORMAL"}
-          setValue={(value) =>
-            setIntegrated(value === "INTÉGRÉ" ? true : false)
-          }
-          options={[
-            ["INTÉGRÉ", "INTÉGRÉ"],
-            ["NORMAL", "NORMAL"],
-          ]}
-        />
-      </div>
-      <textarea
-        value={comments}
-        onChange={(e) => setComments(e.target.value)}
-        placeholder="Commentaire"
-        className="w-full p-2"
-      />
-      <div className="flex flex-wrap items-center justify-center gap-8">
-        <Button
-          type="button"
-          onClick={closePanel}
-          size="sm"
-          icon="HiArrowLeft"
-          variant="white"
-          className="max-w-fit"
-        >
-          Annuler
-        </Button>
-        <Button
-          type="submit"
-          disabled={!identifyingNum}
-          size="sm"
-          icon="RiSave2Fill"
-          className="max-w-fit"
-        >
-          Ajouter
-        </Button>
-      </div>
-    </form>
+        <div className="flex flex-wrap items-center justify-center gap-8">
+          <Button
+            type="button"
+            onClick={closePanel}
+            size="sm"
+            icon="HiArrowLeft"
+            variant="white"
+            className="max-w-fit"
+          >
+            Annuler
+          </Button>
+          <Button
+            type="submit"
+            disabled={!identifyingNum}
+            size="sm"
+            icon="RiSave2Fill"
+            className="max-w-fit"
+          >
+            Ajouter
+          </Button>
+        </div>
+      </form>
+    </>
   )
 }
 
--- src/components/business/tents/TentUpdatePanel.tsx
+++ src/components/business/tents/TentUpdatePanel.tsx
@@ -6,6 +6,7 @@ import { trpc } from "@/utils/trpc"
 import { UIProps } from "@/utils/typedProps"
 import { units } from "@/utils/unit"
 import { Group, State, Unit } from "@prisma/client"
+import Head from "next/head"
 import { FC, FormEvent, useState } from "react"
 import { SingleTent } from "./TentCard"
 import TentInput from "./TentInput"
@@ -63,120 +64,125 @@ const TentUpdatePanel: FC<
   }
 
   return (
-    <form
-      className="mx-auto max-w-[450px] space-y-6 py-4"
-      onSubmit={handleUpdate}
-    >
-      <div className="mx-auto flex h-28 w-28 items-center justify-center rounded-full border-4 border-slate-800">
-        <h2 className="text-3xl font-bold">{tent.identifyingNum}</h2>
-      </div>
-      <div className="pt-4">
-        <div className="mx-auto flex w-fit items-center space-x-2 rounded-lg bg-green-100 py-1 px-2 text-sm font-medium text-green-800 sm:text-base">
-          <Icon name="MdOutlineErrorOutline" className="text-xl" />
-          <span>Pensez à sauvegarder vos modifications</span>
+    <>
+      <Head>
+        <title>{`Modifier la tente ${tent.identifyingNum} | MonMatos`}</title>
+      </Head>
+      <form
+        className="mx-auto max-w-[450px] space-y-6 py-4"
+        onSubmit={handleUpdate}
+      >
+        <div className="mx-auto flex h-28 w-28 items-center justify-center rounded-full border-4 border-slate-800">
+          <h2 className="text-3xl font-bold">{tent.identifyingNum}</h2>
         </div>
-      </div>
+        <div className="pt-4">
+          <div className="mx-auto flex w-fit items-center space-x-2 rounded-lg bg-green-100 py-1 px-2 text-sm font-medium text-green-800 sm:text-base">
+            <Icon name="MdOutlineErrorOutline" className="text-xl" />
+            <span>Pensez à sauvegarder vos modifications</span>
+          </div>
+        </div>
 
-      <div>
-        <p className="text-lg font-bold">Informations</p>
-        <p>Cliquez sur les éléments afin de les modifier</p>
-      </div>
-      <div className="space-y-2">
-        <TentInput
-          label={`Attribué au${unit !== "GROUPE" ? "x" : ""}`}
-          value={unit}
-          setValue={(value) => setUnit(value as Unit)}
-          options={Object.entries(units[movement]).map(([key, value]) => [
-            key as Unit,
-            value,
-          ])}
+        <div>
+          <p className="text-lg font-bold">Informations</p>
+          <p>Cliquez sur les éléments afin de les modifier</p>
+        </div>
+        <div className="space-y-2">
+          <TentInput
+            label={`Attribué au${unit !== "GROUPE" ? "x" : ""}`}
+            value={unit}
+            setValue={(value) => setUnit(value as Unit)}
+            options={Object.entries(units[movement]).map(([key, value]) => [
+              key as Unit,
+              value,
+            ])}
+          />
+          <TentInput
+            label="Taille"
+            value={size.toString()}
+            setValue={(value) => {
+              const int = parseInt(value as string)
+              setSize(int)
+            }}
+            options={[
+              ["1", "1 place"],
+              ["2", "2 places"],
+              ["3", "3 places"],
+              ["4", "4 places"],
+              ["5", "5 places"],
+              ["6", "6 places"],
+              ["8", "8 places"],
+            ]}
+          />
+          <TentInput
+            label="ÉTAT"
+            value={state}
+            setValue={(value) => setState(value as State)}
+            options={[
+              ["BON", "BON"],
+              ["MAUVAIS", "MAUVAIS"],
+              ["INUTILISABLE", "INUTILISABLE"],
+              ["NEUF", "NEUF"],
+            ]}
+          />
+          <TentInput
+            label="Complète ?"
+            value={complete ? "OUI" : "NON"}
+            setValue={(value) => setComplete(value === "OUI" ? true : false)}
+            options={[
+              ["OUI", "OUI"],
+              ["NON", "NON"],
+            ]}
+          />
+          <TentInput
+            label="TYPE"
+            value={type.toUpperCase()}
+            setValue={setType}
+            options={[
+              ["CANADIENNE", "CANADIENNE"],
+              ["QUECHUA", "QUECHUA"],
+              ["MARABOUT", "MARABOUT"],
+            ]}
+          />
+          <TentInput
+            label="Tapis de sol"
+            value={integrated ? "INTÉGRÉ" : "NORMAL"}
+            setValue={(value) =>
+              setIntegrated(value === "INTÉGRÉ" ? true : false)
+            }
+            options={[
+              ["INTÉGRÉ", "INTÉGRÉ"],
+              ["NORMAL", "NORMAL"],
+            ]}
+          />
+        </div>
+        <textarea
+          value={comments}
+          onChange={(e) => setComments(e.target.value)}
+          placeholder="Commentaire"
+          className="w-full p-2"
         />
-        <TentInput
-          label="Taille"
-          value={size.toString()}
-          setValue={(value) => {
-            const int = parseInt(value as string)
-            setSize(int)
-          }}
-          options={[
-            ["1", "1 place"],
-            ["2", "2 places"],
-            ["3", "3 places"],
-            ["4", "4 places"],
-            ["5", "5 places"],
-            ["6", "6 places"],
-            ["8", "8 places"],
-          ]}
-        />
-        <TentInput
-          label="ÉTAT"
-          value={state}
-          setValue={(value) => setState(value as State)}
-          options={[
-            ["BON", "BON"],
-            ["MAUVAIS", "MAUVAIS"],
-            ["INUTILISABLE", "INUTILISABLE"],
-            ["NEUF", "NEUF"],
-          ]}
-        />
-        <TentInput
-          label="Complète ?"
-          value={complete ? "OUI" : "NON"}
-          setValue={(value) => setComplete(value === "OUI" ? true : false)}
-          options={[
-            ["OUI", "OUI"],
-            ["NON", "NON"],
-          ]}
-        />
-        <TentInput
-          label="TYPE"
-          value={type.toUpperCase()}
-          setValue={setType}
-          options={[
-            ["CANADIENNE", "CANADIENNE"],
-            ["QUECHUA", "QUECHUA"],
-            ["MARABOUT", "MARABOUT"],
-          ]}
-        />
-        <TentInput
-          label="Tapis de sol"
-          value={integrated ? "INTÉGRÉ" : "NORMAL"}
-          setValue={(value) =>
-            setIntegrated(value === "INTÉGRÉ" ? true : false)
-          }
-          options={[
-            ["INTÉGRÉ", "INTÉGRÉ"],
-            ["NORMAL", "NORMAL"],
-          ]}
-        />
-      </div>
-      <textarea
-        value={comments}
-        onChange={(e) => setComments(e.target.value)}
-        placeholder="Commentaire"
-        className="w-full p-2"
-      />
-      <div className="flex flex-wrap items-center justify-center gap-8">
-        <Button
-          type="button"
-          onClick={goBackToViewPanel}
-          size="sm"
-          icon="HiArrowLeft"
-          variant="white"
-          className="max-w-fit"
-        >
-          Annuler
-        </Button>
-        <Button
-          type="submit"
-          size="sm"
-          icon="RiSave2Fill"
-          className="max-w-fit"
-        >
-          Sauvegarder
-        </Button>
-      </div>
-    </form>
+        <div className="flex flex-wrap items-center justify-center gap-8">
+          <Button
+            type="button"
+            onClick={goBackToViewPanel}
+            size="sm"
+            icon="HiArrowLeft"
+            variant="white"
+            className="max-w-fit"
+          >
+            Annuler
+          </Button>
+          <Button
+            type="submit"
+            size="sm"
+            icon="RiSave2Fill"
+            className="max-w-fit"
+          >
+            Sauvegarder
+          </Button>
+        </div>
+      </form>
+    </>
   )
 }
 
