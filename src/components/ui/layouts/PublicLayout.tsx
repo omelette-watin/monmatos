3c6f68d85031b8df09cc5e15aa32e7d132788e47
Nicolas <pnwa@icloud.com>
2022-08-31 21:10:35 +0200

    feat: add public layout to homepage

---
+++ src/components/ui/layouts/PublicLayout.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react"
+import Page from "../Page"
+import Header from "../Header"
+
+const PublicLayout = ({
+  children,
+  title,
+}: {
+  children: ReactElement
+  title?: string
+}) => {
+  return (
+    <div className="min-h-screen w-[100vw]">
+      <Header />
+      <Page title={title} className="py-4">
+        {children}
+      </Page>
+    </div>
+  )
+}
+
+export default PublicLayout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import PublicLayout from "@/components/ui/layouts/PublicLayout"
+import { ReactElement } from "react"
 import { NextPageWithLayout } from "./_app"
 
 const HomePage: NextPageWithLayout = () => {
   return <div>Home Page</div>
 }
+
+HomePage.getLayout = (page: ReactElement) => <PublicLayout>{page}</PublicLayout>
 
 export default HomePage
