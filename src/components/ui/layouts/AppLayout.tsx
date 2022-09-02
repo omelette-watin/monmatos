3545c9cea5e778159d17822ab101d414ed34d3dd
Nicolas <pnwa@icloud.com>
2022-09-02 20:17:36 +0200

    fix: check session client side to prevent mobile swipe back errors

--- src/components/ui/layouts/AppLayout.tsx
+++ src/components/ui/layouts/AppLayout.tsx
@@ -1,8 +1,10 @@
-import { ReactElement } from "react"
-import Page from "../Page"
-import Header from "../Header"
 import Notification from "@/components/business/Notification"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { ReactElement, useEffect } from "react"
+import Header from "../Header"
 import Modal from "../modal"
+import Page from "../Page"
 
 const AppLayout = ({
   children,
@@ -11,6 +13,20 @@ const AppLayout = ({
   children: ReactElement
   title?: string
 }) => {
+  const router = useRouter()
+  const { status } = useSession()
+
+  useEffect(() => {
+    router.prefetch("/connexion")
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/connexion")
+    }
+  }, [status, router])
+
   return (
     <div className="min-h-screen w-[100vw]">
       <Header />
