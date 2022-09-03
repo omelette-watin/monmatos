0231e85eed78421c40285ffd2883d35f98fa1141
Nicolas <pnwa@icloud.com>
2022-09-04 00:00:52 +0200

    chore: move tents context

--- src/components/business/hooks/useTentsContext.ts
+++ src/components/business/hooks/useTentsContext.ts
@@ -1,4 +1,4 @@
-import { TentsContext } from "@/components/business/TentsContext"
+import { TentsContext } from "@/components/business/tents/TentsContext"
 import { useContext } from "react"
 
 export const useTentsContext = () => useContext(TentsContext)
