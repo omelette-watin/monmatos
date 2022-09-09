4bfcb9447bda1ccb03fe166e022f5102f022cc44
Nicolas <pnwa@icloud.com>
2022-09-09 17:44:29 +0200

    chore: add register zod schema

--- src/common/validation/auth.ts
+++ src/common/validation/auth.ts
@@ -4,4 +4,10 @@ export const loginSchema = z.object({
   identifier: z.string(),
 })
 
+export const registerSchema = z.object({
+  name: z.string().min(2),
+  movement: z.enum(["AGSE", "SUF", "SGDF"]),
+})
+
+export type IRegister = z.infer<typeof registerSchema>
 export type ILogin = z.infer<typeof loginSchema>
