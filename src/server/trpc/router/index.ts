1c0e9fec64934f9c30be4f48cd332df62fdb3bff
Nicolas <pnwa@icloud.com>
2022-09-09 17:54:02 +0200

    chore: add group router

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,8 +1,10 @@
 import { t } from "../trpc"
+import { groupRouter } from "./group"
 import { tentsRouter } from "./tents"
 
 export const appRouter = t.router({
   tents: tentsRouter,
+  group: groupRouter,
 })
 
 // export type definition of API
