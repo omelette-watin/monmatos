029f34ed7d18b192e9948ba6d5fec42939638849
Nicolas <pnwa@icloud.com>
2022-09-09 17:35:48 +0200

    chore: change wrapper

--- src/components/business/sign/FormWrapper.tsx
+++ src/components/business/sign/FormWrapper.tsx
@@ -2,40 +2,48 @@ import ButtonLink from "@/components/ui/ButtonLink"
 import Logo from "@/components/ui/Logo"
 import type { UIProps } from "@/utils/typedProps"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import type { FC, ReactNode } from "react"
 
 const FormWrapper: FC<
   UIProps<{
     children: ReactNode
-    title: "Me connecter" | "Inscrire mon groupe"
+    title: ReactNode
   }>
 > = ({ children, title }) => {
+  const { asPath } = useRouter()
+  const isConnexionPage = asPath.startsWith("/connexion")
+
   return (
     <div
-      className="bg-main flex min-h-screen w-full flex-col items-center justify-between gap-32 py-8
+      className="bg-main flex min-h-screen w-full flex-col items-center justify-between gap-16 py-8
     px-6"
     >
-      <div className="flex w-full flex-col items-center gap-10 lg:mt-auto">
+      <div className="flex w-full flex-col items-center gap-10 lg:mt-[100px]">
         <div className="self-start lg:hidden">
           <Logo />
         </div>
-        <h1 className="text-4xl font-black sm:text-5xl lg:text-emerald-500">
+        <h1 className="text-center text-4xl font-black sm:text-5xl lg:text-emerald-500">
           {title}
         </h1>
         {children}
       </div>
       <div className="flex flex-col items-center gap-10 text-center text-sm">
-        <div className="flex flex-col gap-4">
-          <p>Toujours pas sur MonMatos ?</p>
+        <div className="flex flex-col items-center gap-4">
+          <p>{`${
+            isConnexionPage
+              ? "Toujours pas sur MonMatos ?"
+              : "Votre groupe est déjà inscrit ?"
+          }`}</p>
 
           <ButtonLink
-            href={title === "Me connecter" ? "/inscription" : "connexion"}
+            href={isConnexionPage ? "/inscription" : "connexion"}
             variant="black"
             size="xs"
             className="max-w-fit"
             icon="FaPen"
           >
-            {title === "Me connecter" ? "Inscrire mon groupe" : "Me connecter"}
+            {isConnexionPage ? "Inscrire mon groupe" : "Me connecter"}
           </ButtonLink>
         </div>
 
