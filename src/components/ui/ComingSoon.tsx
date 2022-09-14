79688fb88ac0a2dac7e9923d2adcdd5db15b0177
Nicolas <pnwa@icloud.com>
2022-09-14 21:46:19 +0200

    chore: change gradient

--- src/components/ui/ComingSoon.tsx
+++ src/components/ui/ComingSoon.tsx
@@ -7,7 +7,7 @@ const ComingSoon: FC<UIProps<{ name: string }>> = ({ name }) => {
     <div className="flex w-full flex-col items-center justify-center pt-20 text-center sm:pt-24 lg:pt-32">
       <h1 className="text-center text-3xl font-extrabold tracking-tight text-slate-900 sm:text-4xl lg:text-5xl">
         Bientôt ici la page "
-        <span className="bg-gradient-to-tl from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
+        <span className="bg-gradient-to-tl from-emerald-400 to-emerald-600 bg-clip-text text-transparent">
           {name}
         </span>
         " . . .
