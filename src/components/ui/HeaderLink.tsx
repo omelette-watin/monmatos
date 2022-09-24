296e6406e1c7c8139f303a692f632d3f62a570cc
Nicolas <pnwa@icloud.com>
2022-09-24 20:56:44 +0200

    chore: add icon

--- src/components/ui/HeaderLink.tsx
+++ src/components/ui/HeaderLink.tsx
@@ -3,17 +3,19 @@ import classNames from "classnames"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { FC, HTMLAttributeAnchorTarget } from "react"
+import Icon, { IconName } from "./Icon"
 
 const HeaderLink: FC<
   UIProps<
     {
       label: string
       href: string
+      icon?: IconName
       target?: HTMLAttributeAnchorTarget
     },
     "a"
   >
-> = ({ href, label, className, target }) => {
+> = ({ href, label, className, target, icon }) => {
   const { asPath } = useRouter()
   const isActive = asPath === href
 
@@ -22,7 +24,7 @@ const HeaderLink: FC<
       <a
         target={target}
         className={classNames(
-          "transition-colors hover:text-emerald-600",
+          "flex items-center gap-2 transition-colors hover:text-emerald-600",
           className,
           {
             "text-black": !isActive,
@@ -30,6 +32,7 @@ const HeaderLink: FC<
           },
         )}
       >
+        {icon && <Icon name={icon} className="w-5" />}
         {label}
       </a>
     </Link>
