7fcc17e6ab66e5fe5f676b12bd734bd1be579c73
Nicolas <pnwa@icloud.com>
2022-09-26 14:49:16 +0200

    fix: toast duration on mobile

--- src/components/ui/AppToaster.tsx
+++ src/components/ui/AppToaster.tsx
@@ -1,4 +1,44 @@
-import toast, { ToastBar, Toaster } from "react-hot-toast"
+import { useEffect } from "react"
+import toast, { Renderable, Toast, ToastBar, Toaster } from "react-hot-toast"
+import Icon from "./Icon"
+
+interface CustomToastBarProps {
+  t: Toast
+  icon: Renderable
+  message: Renderable
+}
+
+const durations: Record<Partial<Toast["type"]>, number> = {
+  success: 3000,
+  error: 4000,
+  loading: 2000,
+  blank: 2000,
+  custom: 2000,
+}
+
+const CustomToastBar = ({ t, icon, message }: CustomToastBarProps) => {
+  useEffect(() => {
+    setTimeout(() => toast.dismiss(t.id), durations[t.type])
+  }, [t.id, t.type])
+
+  return (
+    <>
+      {icon}
+      {message}
+      {t.type !== "loading" ? (
+        <>
+          <Icon name="XIcon" className="w-4" />
+          <div
+            className="absolute inset-0 z-10 cursor-pointer"
+            onClick={() => toast.dismiss(t.id)}
+          />
+        </>
+      ) : (
+        <div className="absolute inset-0 z-10 cursor-progress" />
+      )}
+    </>
+  )
+}
 
 const AppToaster = () => {
   return (
@@ -6,28 +46,17 @@ const AppToaster = () => {
       position="bottom-left"
       toastOptions={{
         success: {
-          duration: 2000,
+          duration: durations.success,
         },
         error: {
-          duration: 3000,
+          duration: durations.error,
         },
       }}
     >
       {(t) => (
         <ToastBar toast={t} position="bottom-center">
           {({ icon, message }) => (
-            <>
-              {icon}
-              {message}
-              {t.type !== "loading" ? (
-                <div
-                  className="absolute inset-0 z-10 cursor-pointer"
-                  onClick={() => toast.dismiss(t.id)}
-                />
-              ) : (
-                <div className="absolute inset-0 z-10 cursor-progress" />
-              )}
-            </>
+            <CustomToastBar t={t} icon={icon} message={message} />
           )}
         </ToastBar>
       )}
