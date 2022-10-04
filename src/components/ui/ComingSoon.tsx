f1a0a79ecce4d2956ec6eb44f6c6ca007f461d47
Nicolas <pnwa@icloud.com>
2022-10-04 21:01:27 +0200

    chore: change coming soon component

--- src/components/ui/ComingSoon.tsx
+++ src/components/ui/ComingSoon.tsx
@@ -6,11 +6,11 @@ const ComingSoon: FC<UIProps<{ name: string }>> = ({ name }) => {
   return (
     <div className="flex w-full flex-col items-center justify-center pt-20 text-center sm:pt-24 lg:pt-32">
       <h1 className="text-center text-3xl font-extrabold tracking-tight text-slate-900 sm:text-4xl lg:text-5xl">
-        Bientôt ici la page "
+        Bientôt ici la page{" "}
         <span className="bg-gradient-to-tl from-emerald-400 to-emerald-600 bg-clip-text text-transparent">
           {name}
-        </span>
-        " . . .
+        </span>{" "}
+        ...
       </h1>
       <p className="mx-auto mt-6 max-w-3xl text-center text-lg text-slate-600">
         Merci de votre patience !
