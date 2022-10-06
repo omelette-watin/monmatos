ad63f474d795692d5eabd8ef832e131c342d4513
Nicolas <pnwa@icloud.com>
2022-10-06 20:12:29 +0200

    chore: better error messages

--- src/common/validation/mail.ts
+++ src/common/validation/mail.ts
@@ -2,16 +2,16 @@ import { z } from "zod"
 
 export const mailPropsSchema = z.object({
   email: z
-    .string({ required_error: "Veuillez entrer votre adresse e-mail." })
-    .email("Veuillez entrer une adresse e-mail valide.")
+    .string({ required_error: "Veuillez entrer votre adresse e-mail" })
+    .email("Veuillez entrer une adresse e-mail valide")
     .trim(),
   message: z
-    .string({ required_error: "Veuillez rentrer votre message." })
-    .max(1000)
+    .string({ required_error: "Veuillez rentrer votre message" })
+    .max(1000, { message: "Veuillez respecter la limite de caractères" })
     .trim(),
   subject: z
-    .string({ required_error: "Veuillez donner un sujet au message." })
-    .max(50)
+    .string({ required_error: "Veuillez donner un sujet à votre message" })
+    .max(50, { message: "Veuillez respecter la limite de caractères" })
     .trim(),
 })
 
