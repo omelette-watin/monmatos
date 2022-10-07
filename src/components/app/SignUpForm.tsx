846202c0110ac1541f89edf2129dfa8bbe44b92d
Nicolas <pnwa@icloud.com>
2022-10-07 14:13:19 +0200

    chore: add disclosure if movement is not present

--- src/components/app/SignUpForm.tsx
+++ src/components/app/SignUpForm.tsx
@@ -4,6 +4,9 @@ import Button from "@/components/ui/Button"
 import Logo from "@/components/ui/Logo"
 import { movements } from "@/utils/records"
 import { trpc } from "@/utils/trpc"
+import { Disclosure } from "@headlessui/react"
+import { ChevronUpIcon } from "@heroicons/react/outline"
+import classNames from "classnames"
 import { Field, Form, Formik } from "formik"
 import { signIn } from "next-auth/react"
 import Head from "next/head"
@@ -95,6 +98,42 @@ const SignUpForm = () => {
                   </Field>
                 </div>
               </div>
+              <Disclosure>
+                {({ open }) => (
+                  <div>
+                    <Disclosure.Button className="-mt-5 flex w-full justify-between rounded-lg bg-blue-50 px-4 py-2 text-left text-sm font-medium text-blue-900 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
+                      <span>Il n'y a pas mon mouvement</span>
+                      <ChevronUpIcon
+                        className={classNames(
+                          "h-5 w-5 transform text-blue-900 transition-transform duration-300",
+                          {
+                            "rotate-180": !open,
+                            "rotate-0": open,
+                          },
+                        )}
+                      />
+                    </Disclosure.Button>
+
+                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm  text-slate-900">
+                      Venez nous en parler sur{" "}
+                      <a
+                        href="https://discord.com/invite/qjvvM6Wya6"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="font-medium text-blue-500"
+                      >
+                        Discord
+                      </a>{" "}
+                      ou envoyez-nous un{" "}
+                      <Link href="/contact">
+                        <a className="font-medium text-blue-500">Message</a>
+                      </Link>
+                      . Nous essayerons d'ajouter votre mouvement et ses
+                      spécificités.
+                    </Disclosure.Panel>
+                  </div>
+                )}
+              </Disclosure>
 
               <Button
                 size="lg"
