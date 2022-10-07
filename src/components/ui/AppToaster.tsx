31af34aa0256ed79638e81374d63f0e247ee3586
Nicolas <pnwa@icloud.com>
2022-10-07 13:13:18 +0200

    fix: loading toaster disappearance

--- src/components/ui/AppToaster.tsx
+++ src/components/ui/AppToaster.tsx
@@ -11,14 +11,16 @@ interface CustomToastBarProps {
 const durations: Record<Partial<Toast["type"]>, number> = {
   success: 3000,
   error: 4000,
-  loading: 2000,
+  loading: 100000,
   blank: 2000,
   custom: 2000,
 }
 
 const CustomToastBar = ({ t, icon, message }: CustomToastBarProps) => {
   useEffect(() => {
-    setTimeout(() => toast.dismiss(t.id), durations[t.type])
+    if (t.type !== "loading") {
+      setTimeout(() => toast.dismiss(t.id), durations[t.type])
+    }
   }, [t.id, t.type])
 
   return (
